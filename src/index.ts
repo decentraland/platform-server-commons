efae459824b742af371dd5f62f43e98798c2f82c
Mariano Goldman <marianogoldman@users.noreply.github.com>
2023-12-07 08:59:05 -0300

    feat: fetch component (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from './adapters'
 export * from './controllers'
 export * from './errors'
 export * from './utils'
---
+++ test/unit/adapters/fetch.spec.ts
@@ -0,0 +1,43 @@
+import fetch from 'node-fetch'
+import { IFetchComponent } from '@well-known-components/http-server'
+import { createFetchComponent } from '../../../src/adapters'
+
+jest.mock('node-fetch', () => jest.fn())
+
+describe('Fetch', () => {
+  let fetcher: IFetchComponent
+
+  beforeEach(async () => {
+    fetcher = await createFetchComponent()
+  })
+
+  it('should parse json correctly', async () => {
+    const mockFetch: jest.Mock = fetch as any
+    mockFetch.mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        status: 200,
+        json: async () => Promise.resolve({ foo: 'bar' })
+      })
+    )
+
+    const response = await fetcher.fetch('http://localhost/something')
+    await expect(response.json()).resolves.toEqual({ foo: 'bar' })
+  })
+
+  it('should throw error on error response', async () => {
+    const mockFetch: jest.Mock = fetch as any
+    mockFetch.mockImplementation(() =>
+      Promise.resolve({
+        ok: false,
+        status: 404,
+        statusText: 'Not found',
+        url: 'http://localhost/something'
+      })
+    )
+
+    await expect(fetcher.fetch('http://localhost/something')).rejects.toThrow(
+      'HTTP Error Response: 404 Not found for URL http://localhost/something'
+    )
+  })
+})
