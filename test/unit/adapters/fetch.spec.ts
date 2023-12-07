2a5a9bdaa03464458e7a5925af9266b8f7e38a6b
Mariano Goldman <mariano.goldman@decentraland.org>
2023-12-07 08:57:52 -0300

    lint

--- test/unit/adapters/fetch.spec.ts
+++ test/unit/adapters/fetch.spec.ts
@@ -13,25 +13,31 @@ describe('Fetch', () => {
 
   it('should parse json correctly', async () => {
     const mockFetch: jest.Mock = fetch as any
-    mockFetch.mockImplementation(()=> Promise.resolve({
-      ok: true,
-      status: 200,
-      json: async () => Promise.resolve({ foo: 'bar' }),
-    }))
+    mockFetch.mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        status: 200,
+        json: async () => Promise.resolve({ foo: 'bar' })
+      })
+    )
 
-    let response = await fetcher.fetch('http://localhost/something');
+    const response = await fetcher.fetch('http://localhost/something')
     await expect(response.json()).resolves.toEqual({ foo: 'bar' })
   })
 
   it('should throw error on error response', async () => {
     const mockFetch: jest.Mock = fetch as any
-    mockFetch.mockImplementation(()=> Promise.resolve({
-      ok: false,
-      status: 404,
-      statusText: 'Not found',
-      url: 'http://localhost/something'
-    }))
+    mockFetch.mockImplementation(() =>
+      Promise.resolve({
+        ok: false,
+        status: 404,
+        statusText: 'Not found',
+        url: 'http://localhost/something'
+      })
+    )
 
-    await expect(fetcher.fetch('http://localhost/something')).rejects.toThrow('HTTP Error Response: 404 Not found for URL http://localhost/something')
+    await expect(fetcher.fetch('http://localhost/something')).rejects.toThrow(
+      'HTTP Error Response: 404 Not found for URL http://localhost/something'
+    )
   })
 })
